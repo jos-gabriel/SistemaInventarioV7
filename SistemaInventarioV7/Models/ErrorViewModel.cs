85100d5c5a7e68585c4a389d0558466d6c73b1be
jos-gabriel <jos.gabrie@gmail.com>
2023-06-03 01:12:40 -0600

    Initial commit

---
+++ SistemaInventarioV7/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SistemaInventarioV7.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
